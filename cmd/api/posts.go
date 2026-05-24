655875eb65ec1df61a3e0bf52bb985a4c0288156
leonfarhan <leonfarhan404@gmail.com>
2026-05-24 23:13:49 +0700

    feat: implement GET endpoint to retrieve posts by ID

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/Leonfarhan/simple-social/internal/store"
+	"github.com/go-chi/chi/v5"
 )
 
 type CreatePostPayload struct {
-	Title   string `json:"title"`
-	Content string `json:"content"`
-	Tags []string `json:"tags"`
+	Title   string   `json:"title"`
+	Content string   `json:"content"`
+	Tags    []string `json:"tags"`
 }
 
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var payload CreatePostPayload
+	ctx := r.Context()
 
 	if err := readJSON(w, r, &payload); err != nil {
 		writeJSONError(w, http.StatusBadRequest, err.Error())
@@ -21,14 +25,12 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	post := &store.Post{
-		Title: payload.Title,
+		Title:   payload.Title,
 		Content: payload.Content,
-		Tags: payload.Tags,
+		Tags:    payload.Tags,
 		// change after auth for this user id
 		UserID: 1,
 	}
-
-	ctx := r.Context()
 
 	if err := app.store.Posts.Create(ctx, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
@@ -36,6 +38,33 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	id, err := strconv.ParseInt(chi.URLParam(r, "postID"), 10, 64)
+	if err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	post, err := app.store.Posts.GetByID(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+
+		return
+	}
+
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
